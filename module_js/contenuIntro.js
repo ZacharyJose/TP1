7c4181b2e01709cd51322dc7efddcfd4e6cedeea
ZacharyJose <e1646855@cmaisonneuve.qc.ca>
2019-02-20 13:51:14 -0500

    changer le menu de navigation

---
+++ module_js/contenuIntro.js
@@ -0,0 +1,10 @@
+/* Contenu intro est un objet litéral qui contiendra 
+    les éléments d'introduction
+*/
+
+export const contenuIntro = {
+	titrePrincipal : "Les personnages principaux",
+	titreSecondaire : "Joel",
+	titreTernaire : "Ellie",
+	description : " Veuillez cliquer sur un prénom afin d'obtenir plus d'information sur le personnage en question"
+}
